31be6dcfd689d3087ddae82dd85cb1e507050ed8
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-03-15 23:53:48 +0530

    Highlighting selected page functionality added

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  AfterViewInit
+} from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.sass"]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   @Output() public openModal = new EventEmitter();
+  selectedPage = "Migration strategy";
   menuItems = [
     {
       src: "../../../assets/logo/instantParameter.png",
@@ -42,7 +49,31 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  ngAfterViewInit() {
+    // to highlight the selected page in the initial load
+    this.addPageBorderBottom(this.selectedPage);
+  }
+
+  // to Highlight the selected Page
+  addPageBorderBottom(selectedPage) {
+    let element = document.getElementById(selectedPage);
+    element.style.borderBottom = "solid 5px";
+  }
+  // to remove highlighting
+  removePageBorderBottom(selectedPage) {
+    let element = document.getElementById(selectedPage);
+    element.style.borderBottom = "";
+  }
   public onToggleSidenav = () => {
     this.openModal.emit();
   };
+
+  differentPageSelected(selectedPage) {
+    // won't trigger if the selected page is clicked
+    if (selectedPage !== this.selectedPage) {
+      this.removePageBorderBottom(this.selectedPage);
+      this.selectedPage = selectedPage;
+      this.addPageBorderBottom(this.selectedPage);
+    }
+  }
 }
